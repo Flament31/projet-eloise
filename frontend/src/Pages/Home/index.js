22b563b1cbe33002f805ee064ac69c225ac3f033
Baudouin Flament <baudouinflament@gmail.com>
2022-06-10 19:13:00 +0200

    initial commit

---
+++ frontend/src/Pages/Home/index.js
@@ -0,0 +1,14 @@
+const Home = () => {
+    return (
+
+        <div className="main">
+            <div className="row justify-content-center">
+                <h1 className="h1" >Bienvenue sur le site d'Eloïse Molinié !</h1>
+            </div>
+        </div>
+
+
+    );
+};
+
+export default Home
